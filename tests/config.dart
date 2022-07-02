13747690da04511c51ee846b124f39039ed6bfa6
Facio <66415135+maciej-debowski@users.noreply.github.com>
2022-07-02 22:46:58 +0200

    Update config.dart

--- tests/config.dart
+++ tests/config.dart
@@ -2,9 +2,10 @@ class Config {
   String title = "super app";
 
   // Dart somehow close localhost connection with database so please use external database
+  // This is test database.
   String database_host = "54.38.50.59";
   String database_user = "www7803_target";
-  String database_pass = "yeZDCG9dkRrK9xKP394d";
+  String database_pass = "----";
   String database_name = "www7803_target";
   int database_port = 3306;
   int database_max_conns = 10;
